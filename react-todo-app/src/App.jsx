36ac5674397d3f0a3e7046f71d49e9059df8a0c8
snehchakrapani <snehchakrapani2002@gmail.com>
2026-03-11 14:55:19 +0530

    added input box and button and their css

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -2,9 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
+    const [inpuValue, setInputValue] = useState('')
   return (
     <div className="container">
       <h1>Todo App</h1>
+
+      <div className = "input-box">
+        <input 
+        type = "text"
+        placeholder = "Enter todo..."
+        value={inpuValue}
+        onChange={(e) => setInputValue(e.target.value)}
+    />
+    <button>Add</button>
+    </div>
     </div>
   )
 }
